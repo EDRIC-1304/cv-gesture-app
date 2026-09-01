1e4e3bca5307af0ce6c5418cbb99663c8229f4c6
edric <edriccoelho04@gmail.com>
2026-09-01 12:58:54 +0530

    Add gesture stability detection

---
+++ utils/gestureStability.js
@@ -0,0 +1,34 @@
+let lastGesture = "none";
+let stableGesture = "none";
+let gestureStartTime = 0;
+
+const STABILITY_TIME = 300;
+
+export function getStableGesture(gesture) {
+  const now = performance.now();
+
+  if (gesture !== lastGesture) {
+    lastGesture = gesture;
+    gestureStartTime = now;
+    return stableGesture;
+  }
+
+  if (
+    gesture !== "none" &&
+    now - gestureStartTime >= STABILITY_TIME
+  ) {
+    stableGesture = gesture;
+  }
+
+  if (gesture === "none") {
+    stableGesture = "none";
+  }
+
+  return stableGesture;
+}
+
+export function resetGestureStability() {
+  lastGesture = "none";
+  stableGesture = "none";
+  gestureStartTime = 0;
+}
